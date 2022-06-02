0c90dae7611ece57cca9bd8fda8145871ff44579
andrey <andrey.kozhevnikov@devexpress.com>
2022-06-02 18:18:24 +0400

    exists

--- ConsoleApp1/dxTestSolutionXPO/Tests/BaseTest.cs
+++ ConsoleApp1/dxTestSolutionXPO/Tests/BaseTest.cs
@@ -43,6 +43,18 @@ namespace dxTestSolutionXPO.Tests {
             var t31 = ConnectionHelper.AddOrderItem(uow, c3, "Task3-1", 400);
             uow.CommitChanges();
         }
+        public void PopulateSelectFromCollectionForCount() {
+            ConnectionHelper.Connect(DevExpress.Xpo.DB.AutoCreateOption.DatabaseAndSchema);
+            var uow = new UnitOfWork();
+            var c0 = ConnectionHelper.AddOrder(uow, "FirstName0");
+            var t00 = ConnectionHelper.AddOrderItem(uow, c0, "Task0-0", 10);
+            var c1 = ConnectionHelper.AddOrder(uow, "FirstName1");
+            var c2 = ConnectionHelper.AddOrder(uow, "FirstName2");
+            var t20 = ConnectionHelper.AddOrderItem(uow, c2, "Task2-0", 10);
+            var t21 = ConnectionHelper.AddOrderItem(uow, c2, "Task2-0", 20);
+            uow.CommitChanges();
+        }
+
         public void PopulateDiffItems() {
             ConnectionHelper.Connect(DevExpress.Xpo.DB.AutoCreateOption.DatabaseAndSchema);
             var uow = new UnitOfWork();
