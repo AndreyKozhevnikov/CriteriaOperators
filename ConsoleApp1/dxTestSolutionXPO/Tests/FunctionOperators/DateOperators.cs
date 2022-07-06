7094f969555c87a01081009c02df1d1dd6f662e8
andrey <andrey.kozhevnikov@devexpress.com>
2022-07-06 16:44:22 +0400

    add test with parse+date

--- ConsoleApp1/dxTestSolutionXPO/Tests/FunctionOperators/DateOperators.cs
+++ ConsoleApp1/dxTestSolutionXPO/Tests/FunctionOperators/DateOperators.cs
@@ -60,5 +60,53 @@ namespace dxTestSolutionXPO.Tests.FunctionOperators {
             Assert.AreEqual("Order22", resColl[0].OrderName);
             Assert.AreEqual("Order33", resColl[1].OrderName);
         }
+
+
+        [Test]
+        public void Test1_0() {
+            //arrange
+            PopulateForDates();
+            var uow = new UnitOfWork();
+            //act
+            CriteriaOperator criterion = CriteriaOperator.Parse("[OrderDate]=?", new DateTime(2022, 3, 10));
+            var xpColl = new XPCollection<Order>(uow);
+            xpColl.Filter = criterion;
+            var resColl = xpColl.OrderBy(x => x.OrderName).ToList();
+            var result3 = resColl.Count;
+            //assert
+            Assert.AreEqual(1, result3);
+            Assert.AreEqual("Order22", resColl[0].OrderName);
+        }
+        [Test]
+        public void Test1_1() {
+            //arrange
+            PopulateForDates();
+            var uow = new UnitOfWork();
+            //act
+            CriteriaOperator criterion = new BinaryOperator(nameof(Order.OrderDate), new DateTime(2022, 3, 10));
+            var xpColl = new XPCollection<Order>(uow);
+            xpColl.Filter = criterion;
+            var resColl = xpColl.OrderBy(x => x.OrderName).ToList();
+            var result3 = resColl.Count;
+            //assert
+            Assert.AreEqual(1, result3);
+            Assert.AreEqual("Order22", resColl[0].OrderName);
+        }
+        [Test]
+        public void Test1_2() {
+            //arrange
+            PopulateForDates();
+            var uow = new UnitOfWork();
+            //act
+            var requiredDate = new DateTime(2022, 3, 10);
+            CriteriaOperator criterion = CriteriaOperator.FromLambda<Order>(o => o.OrderDate == requiredDate);
+            var xpColl = new XPCollection<Order>(uow);
+            xpColl.Filter = criterion;
+            var resColl = xpColl.OrderBy(x => x.OrderName).ToList();
+            var result3 = resColl.Count;
+            //assert
+            Assert.AreEqual(1, result3);
+            Assert.AreEqual("Order22", resColl[0].OrderName);
+        }
     }
 }
